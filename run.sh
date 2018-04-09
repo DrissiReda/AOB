b0cf989d9043e0c3b9cc0aee6e09acc5f7072179
drissiReda <mohamedreda.drissi@edu.uca.ma>
2018-04-09 14:30:26 +0200

    Added median file and graphics

--- run.sh
+++ run.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-
-
+mkdir -p graphics
+echo '' > med.csv
+i=$1
+#tail -n +2 "$i" > "$i.tmp" && mv "$i.tmp" "$i"
+z=$(sort -n $i | sed -ne "$(($((wc -l))/2+1))p")
+y=$(echo "$i"| sed 's/\.[^.]*$//' | sed 's/\.[^.]*$//')
+echo "median is $z"
+echo "$y $z" >> med.csv
+#cat -n "$i" > "$i.tmp" && mv "$i.tmp" "$i"
+gnuplot -e "set terminal png ; set output 'graphics/$y.png' ; plot '$i' with line ;"
+x=$1
+shift
 for i in $@ ; do
-	tail -n +2 "$i" > "$i.tmp" && mv "$i.tmp" "$i"
+	#tail -n +2 "$i" > "$i.tmp" && mv "$i.tmp" "$i"
 	z=$(sort -n $i | sed -ne "$(($((wc -l))/2+1))p")
+	y=$(echo "$i"| sed 's/\.[^.]*$//' | sed 's/\.[^.]*$//')
 	echo "median is $z"
-	gnuplot -e "set terminal png ; set output '$i.png' ; plot '$i' with line"
+	echo "$y $z" >> med.csv
+	#cat -n "$i" > "$i.tmp" && mv "$i.tmp" "$i"
+	gnuplot -e "set terminal png ; set output 'graphics/$y.png' ; plot '$x' with line, '$i' with line"
+	x=$i
 done
