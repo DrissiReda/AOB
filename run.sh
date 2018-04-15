5c5802d5664036c16a283ff547b55b333143edd4
drissiReda <mohamedreda.drissi@edu.uca.ma>
2018-04-15 20:13:12 +0200

    faster script

--- run.sh
+++ run.sh
@@ -12,14 +12,8 @@ echo '' > med.csv
 # x=$1
 # shift
 for i in $@ ; do
-	awk '{print $NF}' $i | sort | cut -f2- -d' ' > $i.tmp && mv $i.tmp $i
-	fnan=$(head -n +1 $i | awk -F ' *' '$NF ~ /^[^0-9].*$/ { print $NF }')
-	lnan=$(tail -n1 $i | awk -F ' *' '$NF ~ /^[^0-9].*$/ { print $NF }')
-	if [ ! -z "$fnan" ]; then #first line is nan
-		tail -n +2 "$i" > "$i.tmp" && mv "$i.tmp" "$i"
-	elif [ ! -z "$lnan" ]; then #last line is nan
-		sed '$d' "$i" >  "$i.tmp" && mv "$i.tmp" "$i"
-	fi
+	awk '{print $NF}' $i | sort | cut -f2- -d' ' > $i.tmp && mv $i.tmp $i # remove other fields
+	awk -F ' *' '$NF ~ /^[0-9].*$/ { print $NF }' $i | sort  > $i.tmp && mv $i.tmp $i # remove nan lines
 	z=$(sort -n $i | sed -ne "$(($((wc -l))/2+1))p")
 	y=$(echo "$i"| sed 's/\.[^.]*$//' | sed 's/\.[^.]*$//')
 	echo "median is $z"
